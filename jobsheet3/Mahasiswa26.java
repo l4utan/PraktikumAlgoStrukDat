6b6696ac94b512fc89354058c94e8df398ea277e
l4utan <espressvre29@gmail.com>
2026-03-08 20:04:05 +0700

    Modifikasi Percobaan 2

--- jobsheet3/Mahasiswa26.java
+++ jobsheet3/Mahasiswa26.java
@@ -6,4 +6,12 @@ public class Mahasiswa26 {
     public String kelas;
     public float ipk;
 
+    public void cetakInfo(){
+        System.out.println("NIM     :" + nim);
+        System.out.println("Nama    :" + nama);
+        System.out.println("Kelas   :" + kelas);
+        System.out.println("IPK     :" + ipk);
+        System.out.println("-------------------------------------");
+    }
+
 }
