ccb57fca5e924ec3a2cb0ace908c3caaaecea552
l4utan <espressvre29@gmail.com>
2026-03-12 14:00:06 +0700

    Modifikasi Percobaan 1

--- jobsheet5/Faktorial.java
+++ jobsheet5/Faktorial.java
@@ -1,8 +1,10 @@
 public class Faktorial {
     int faktorialBF(int n) {
         int fakto = 1;
-        for (int i = 1; i<=n; i++) {
-            fakto = fakto * i;
+        int i = 1;
+        while (i <= n) {
+            fakto = fakto*i;
+            i++;
         }
         return fakto;
     }
