6ede03bbeb1451970a878b2c5f31931b9654b7c2
l4utan <espressvre29@gmail.com>
2026-04-13 16:09:38 +0700

    Modifikasi Praktikum 2

--- jobsheet6/MahasiswaDemo25.java
+++ jobsheet6/MahasiswaDemo25.java
@@ -1,19 +1,27 @@
+import java.util.Scanner;
+
 public class MahasiswaDemo25 {
     public static void main(String[] args) {
         MahasiswaBerprestasi25 list = new MahasiswaBerprestasi25();
-        Mahasiswa25 m1 = new Mahasiswa25("123", "Zidan", "2A", 3.2);
-        Mahasiswa25 m2 = new Mahasiswa25("124", "Ayu", "2A", 3.5);
-        Mahasiswa25 m3 = new Mahasiswa25("125", "Sofi", "2A", 3.1);
-        Mahasiswa25 m4 = new Mahasiswa25("126", "Sita", "2A", 3.9);
-        Mahasiswa25 m5 = new Mahasiswa25("127", "Miki", "2A", 3.7);
+        Scanner sc = new Scanner(System.in);
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println("=====INPUT MAHASISWA KE-" + (i+1) + "=====");
+            System.out.print("NIM: ");
+            String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
+            System.out.println("IPK: ");
+            double ipk = Double.parseDouble(sc.nextLine());
 
-        list.tambah(m1);
-        list.tambah(m2);
-        list.tambah(m3);
-        list.tambah(m4);
-        list.tambah(m5);
+            Mahasiswa25 m = new Mahasiswa25(nim, nama, kelas, ipk);
+            list.tambah(m);
+        }
 
+        sc.close();
+        
         System.out.println("Data mahasiswa sebelum sorting: ");
         list.tampil();
 
