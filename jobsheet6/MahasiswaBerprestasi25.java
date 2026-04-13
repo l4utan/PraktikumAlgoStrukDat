e16c23c5abc582a5b5d339dbe14831fc79272bd1
l4utan <espressvre29@gmail.com>
2026-04-13 16:37:30 +0700

    Modifikasi Praktikum 4

--- jobsheet6/MahasiswaBerprestasi25.java
+++ jobsheet6/MahasiswaBerprestasi25.java
@@ -50,7 +50,7 @@ public class MahasiswaBerprestasi25 {
         for (int i = 1; i < listMhs.length; i++) {
             Mahasiswa25 tmp = listMhs[i];
             int j = i;
-            while (j > 0 && listMhs[j-1].ipk > tmp.ipk) {
+            while (j > 0 && listMhs[j-1].ipk < tmp.ipk) {
                 listMhs[j] = listMhs[j-1];
                 j--;
             }
