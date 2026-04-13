ace27ceac77030b1c32f2958850f58224151b714
l4utan <espressvre29@gmail.com>
2026-04-13 16:01:38 +0700

    Praktikum 1

---
+++ jobsheet6/SortingMain25.java
@@ -0,0 +1,32 @@
+public class SortingMain25 {
+    public static void main(String[] args) {
+        int a[] = {20, 10, 2, 7, 12};
+        int b[] = {30, 20, 2, 8, 14};
+        int c[] = {40, 10, 4, 9, 3};
+
+        Sorting25 dataurut1 = new Sorting25(a, a.length);
+
+        System.out.println("Data awal 1");
+        dataurut1.tampil();
+        dataurut1.bubbleSort();
+        System.out.println("Data sudah diurutkan dengan BUBLE SORT (ASC)");
+        dataurut1.tampil();
+
+        Sorting25 dataurut2 = new Sorting25(b, b.length);
+
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
+        
+        Sorting25 dataurut3 = new Sorting25(c, c.length);
+        
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut2.tampil();
+
+    }
+}
