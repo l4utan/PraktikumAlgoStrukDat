38f4f220a013a9aba1a01fb7b3d1cd40b1f6ca0e
l4utan <espressvre29@gmail.com>
2026-05-11 20:56:15 +0700

    Percobaan 2

--- jobsheet9/StackTugasMahasiswa25.java
+++ jobsheet9/StackTugasMahasiswa25.java
@@ -73,4 +73,18 @@ public class StackTugasMahasiswa25 {
     public int count() {
         return top + 1;
     }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi25 stack = new StackKonversi25();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
