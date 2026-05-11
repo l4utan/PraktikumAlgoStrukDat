a3e1531b0b10e25d3cb29177e898763a259afba6
l4utan <espressvre29@gmail.com>
2026-05-11 21:09:15 +0700

    Modifikasi percobaan 2

--- jobsheet9/StackTugasMahasiswa25.java
+++ jobsheet9/StackTugasMahasiswa25.java
@@ -76,7 +76,7 @@ public class StackTugasMahasiswa25 {
 
     public String konversiDesimalKeBiner(int nilai) {
         StackKonversi25 stack = new StackKonversi25();
-        while (nilai > 0) {
+        while (nilai != 0) {
             int sisa = nilai % 2;
             stack.push(sisa);
             nilai = nilai / 2;
