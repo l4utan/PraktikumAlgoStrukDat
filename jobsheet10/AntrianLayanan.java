c688f87664ad13a743dfac17d616f65f549f7a8d
l4utan <espressvre29@gmail.com>
2026-05-17 18:08:41 +0700

    Modifikasi Percobaan 2

--- jobsheet10/AntrianLayanan.java
+++ jobsheet10/AntrianLayanan.java
@@ -89,4 +89,14 @@ public class AntrianLayanan {
     public int getJumlahAntrian() {
         return size;
     }
+
+    public void lihatAkhir() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong");
+        } else {
+            System.out.print("Mahasiswa paling belakang: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[rear].tampilkanData();
+        }
+    }
 }
--- jobsheet10/LayananAkademikSIAKAD.java
+++ jobsheet10/LayananAkademikSIAKAD.java
@@ -13,6 +13,7 @@ public class LayananAkademikSIAKAD {
             System.out.println("3. Lihat Mahasiswa Terdepan");
             System.out.println("4. Lihat Semua Antrian");
             System.out.println("5. Jumlah Mahasiswa dalam Antrian");
+            System.out.println("6. Cek Antrian paling belakang");
             System.out.println("0. Keluar");
             System.out.print("Pilih menu: ");
             pilihan = sc.nextInt(); sc.nextLine();
@@ -49,6 +50,10 @@ public class LayananAkademikSIAKAD {
 
                 case 5:
                     System.out.println("Jumlah dalam antrian: " + antrian.getJumlahAntrian());
+                    break;
+
+                case 6:
+                    antrian.lihatAkhir();
                     break;
 
                 case 0:
