7845ef49f0f625cf72657989bac0a150b90e3c90
l4utan <espressvre29@gmail.com>
2026-05-25 11:41:20 +0700

    Percobaan 2

--- jobsheet11/SLLMain.java
+++ jobsheet11/SLLMain.java
@@ -31,5 +31,17 @@ public class SLLMain {
 
         sll.print();
         sc.close();
+
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
     }
 }
